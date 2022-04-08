91a56426fc1b779500d058743485fedbc15a15d8
Zachary Thompson <zgthomps3@gmail.com>
2022-04-08 16:29:47 -0600

    basic product form layout

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -4,7 +4,29 @@ import ProductCard from './ProductCard.js'
 function ProductForm() {
 	const { id } = useParams();
 	
-	return <p>Temporary: { id }</p>
+	return (
+		<div style={{ display: 'flex', justifyContent: 'center', gap: '1rem' }}>
+			<form>
+				<div>
+					<label>Product Image</label>
+					<div style={{ height: '3rem', borderStyle: 'solid' }} />
+				</div>
+				<div>
+					<label htmlFor='name'>Product Name</label>
+					<input type='text' name='name' id='name' />
+				</div>
+				<div>
+					<label htmlFor='price'>Product Price</label>
+					<input type='number' name='price' id='price' min='0.01' step='0.01' />
+				</div>
+				<div>
+					<label htmlFor='description'>Product Description</label>
+					<input type='text' name='description' id='description' />
+				</div>
+			</form>
+			<ProductCard name={id} image={'/placeholder.png'} />
+		</div>
+	);
 }
 
 export default ProductForm
