5884b88d344dfd91623bbb393b8f5413432440cd
Zachary Thompson <zgthomps3@gmail.com>
2022-04-08 12:50:18 -0600

    hide secrets

--- src/index.js
+++ src/index.js
@@ -10,18 +10,20 @@ import Panels from './components/Panels.js'
 import ProductCard from './components/ProductCard.js'
 import ProtectedRoute from './components/ProtectedRoute.js'
 import { initializeApp } from 'firebase/app'
+import { getDatabase } from 'firebase/database'
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAp91rHZCuditAMUgKRff5ezTe8cA3q26k",
-  authDomain: "storefront-445bf.firebaseapp.com",
-  databaseURL: "https://storefront-445bf-default-rtdb.firebaseio.com",
-  projectId: "storefront-445bf",
-  storageBucket: "storefront-445bf.appspot.com",
-  messagingSenderId: "803238398469",
-  appId: "1:803238398469:web:74c847274b8b0eb22f6a6c"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 const app = initializeApp(firebaseConfig);
+export const db = getDatabase();
 export const auth = getAuth(app);
 
 
