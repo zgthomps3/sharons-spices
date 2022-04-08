42983bfbcd5bde019bb009ce82596a28034b2512
Zachary Thompson <zgthomps3@gmail.com>
2022-04-08 13:34:55 -0600

    useGetAllProducts hook

---
+++ src/hooks/useGetAllProducts.js
@@ -0,0 +1,22 @@
+import { db } from '../index.js'
+import { ref, onValue } from 'firebase/database'
+import React, { useState, useEffect } from 'react'
+
+function useGetAllProducts () {
+	const [products, setProducts] = useState(null);
+	
+	const productsRef = ref(db, 'products');
+	
+	useEffect(() => {
+		onValue(productsRef, (snapshot) => {
+			setProducts(snapshot.val());
+		});
+		return () => {
+			productsRef.off();
+		};
+	});
+	
+	return products;
+}
+
+export default useGetAllProducts;
